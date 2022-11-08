7c92fcb62963703e20304a4e329c71a29a3f6b86
Magdiel <magdiel.altynbekov@gmail.com>
2022-11-08 18:22:38 +0600

    Initial Commit

---
+++ HomeWork InterFace 1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HomeWork InterFace 1
+//
+//  Created by Magdiel Altynbekov on 8/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
